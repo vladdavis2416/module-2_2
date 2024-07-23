362093cc5c6ec18ebedc9da24d77945aac33586d
vladdavis2416 <sockolow.vladisl@gmail.com>
2024-07-23 16:20:53 +0300

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -3,5 +3,5 @@ second= input("Введите второе число:")
 third= input("Введите третье число:")
 
 if first==second==third:    print(3)
-if first==second or second==third or first==third:   print(2)
+elif first==second or second==third or first==third:   print(2)
 else: print(0)
